a8a849f4b488bfbb146042a1b947ed62b80f3122
Dipali pradip Lavate <lavatedipali05@gmail.com>
2026-05-03 15:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import streamlit as st   
+
+st.set_page_config(page_title="Stock Dashboard", layout="wide")  
+
+import yfinance as yf
+import pandas as pd
+import numpy as np
 import streamlit as st
 import pandas as pd
 
